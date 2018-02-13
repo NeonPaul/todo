97299b1e44d64ff54f0de45e4b907d177a95b1ef
Paul Kiddle <p.kiddle@nhm.ac.uk>
2018-02-13 11:17:02 +0000

    Style controls

--- src/components/item.js
+++ src/components/item.js
@@ -1,6 +1,9 @@
 const marked = require("marked");
 const status = require('../status')
 const withCss = require('../mixins/with-css')
+const bemHelper = require('../utils/bem-helper')
+
+const bem = bemHelper('Item')
 
 module.exports = {
   mixins: [withCss(`
@@ -8,17 +11,33 @@ module.exports = {
     display: flex;
     align-items: flex-start;
   }
-  
+
+  .Item__move svg {
+    width: 1.5em;
+  }
+
   .Item__move {
-    margin: 5px 0;
+    margin-top: -0.25em;
   }
 
   .Item__move form {
     margin: 0;
   }
 
-  .Item__move form button {
-    font-size: 50%;
+  .Item button {
+    background: transparent;
+    border: none;
+    opacity: 0.5;
+    cursor: pointer;
+    padding: 0;
+  }
+
+  .Item__complete svg {
+    width: 1.5em;
+  }
+
+  .Item button:hover {
+    opacity: 1;
   }
   `)],
   props: ['i', 'pt', 'nt'],
@@ -29,18 +48,30 @@ module.exports = {
   },
   template: `
   <div class="Item">
-    <form :action="'/'+i.id" method="post">
-      <button>x</button>
+    <form :action="'/'+i.id" method="post" class="${bem('complete')}">
+      <button>
+        <svg viewbox="0 0 4 4">
+          <path d="M 1,1 L 3,3 M 3,1 L 1,3" stroke="currentColor" stroke-width="0.6" />
+        </svg>
+      </button>
     </form>
     <div class="Item__move">
       <form action="/order" method="post">
-        <button :disabled="!pt">▲</button>
+        <button :disabled="!pt">
+          <svg viewbox="0 0 4 2.5">
+            <path d="M 1,2 L2,1 L3,2" stroke="currentColor" stroke-width="0.6" />
+          </svg>
+        </button>
         <input type="hidden" name="set" :value="i.id+'='+(i.weight - 1)">
         <input type="hidden" name="set" :value="(pt && pt.id) + '=' + (pt &&
   pt.weight + 1)">
       </form>
       <form action="/order" method="post">
-        <button :disabled="!nt">▼</button>
+        <button :disabled="!nt">
+          <svg viewbox="0 0 4 2.5">
+            <path d="M 1,0.5 L2,1.5 L3,0.5" stroke="currentColor" stroke-width="0.6" />
+          </svg>
+        </button>
         <input type="hidden" name="set" :value="i.id + '=' + (i.weight + 1)">
         <input type="hidden" name="set" :value="(nt && nt.id) + '=' + (nt &&
   nt.weight - 1)">
