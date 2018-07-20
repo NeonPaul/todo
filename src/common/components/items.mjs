d2aee49d4ce20b92ac9fa3bc83ac53bc183befb5
Paul <p.kiddle@nhm.ac.uk>
2018-07-20 14:22:45 +0100

    Don't drag inputs

--- src/common/components/items.mjs
+++ src/common/components/items.mjs
@@ -16,7 +16,7 @@ export default {
       })
     }
   },
-  template: `<div><draggable :value="items" @change="change">
+  template: `<div><draggable :value="items" @change="change" :options="{ filter: 'textarea, input, button, select', preventOnFilter: false }">
     <Item v-for="(item, index) in items" :key="item.id" :i="item" :status="status" :first="index === 0" :last="index === items.length - 1"/><br>
   </draggable></div>`
 }
